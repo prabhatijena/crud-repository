a4ba0a7033f969a9b3856d8e73651ee33a1dc575
prabhatijena <jenaprabhati97@gmail.com>
2025-07-28 14:16:41 +0530

    Update CrudPatientApplication.java

--- src/main/java/com/coolcoder/CrudPatientApplication.java
+++ src/main/java/com/coolcoder/CrudPatientApplication.java
@@ -11,7 +11,7 @@ public class CrudPatientApplication {
 
                 int i = 10;
                 int j = 20;
-
+                String s1 = "Bipin";
                 String s = "Prabhati";
 		boolean b = true;
 
